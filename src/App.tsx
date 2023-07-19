126ba5eb8acbb0a4cb5c8f65fef36326cdf69ca3
Sin-god <illya.komarnickii1@gmail.com>
2023-07-19 18:49:19 +0300

    Add pizza create component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Admin, DataProvider, Loading, Resource } from "react-admin";
 import buildHasuraProvider from "ra-data-hasura";
 import { MenuList } from "./modules/menu/components/menu-list/menu-list.component";
 import { MenuEdit } from "./modules/menu/components/menu-edit/menu-edit.component";
+import { MenuCreate } from "./modules/menu/components/menu-create/menu-create.component";
 
 export const App = () => {
   const [dateProvider, setDataProvider] = useState<DataProvider<string> | null>(
@@ -27,7 +28,12 @@ export const App = () => {
     <>
       <CssBaseline />
       <Admin dataProvider={dateProvider}>
-        <Resource name="menu" list={MenuList} edit={MenuEdit} />
+        <Resource
+          name="menu"
+          list={MenuList}
+          edit={MenuEdit}
+          create={MenuCreate}
+        />
       </Admin>
     </>
   );
