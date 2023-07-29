8fc9887453bcbbad6fce9daf9523e43dd62a3ef8
Sin-god <illya.komarnickii1@gmail.com>
2023-07-29 15:37:57 +0300

    Admin change

--- src/modules/menu/components/menu-edit/menu-edit.component.tsx
+++ src/modules/menu/components/menu-edit/menu-edit.component.tsx
@@ -1,14 +1,23 @@
-import { Edit, NumberInput, SimpleForm, TextInput } from "react-admin";
+import { Menu } from "@app/core/types";
+import {
+  Edit,
+  NumberInput,
+  SimpleForm,
+  TextInput,
+  useRecordContext,
+} from "react-admin";
+import { MenuEditTitle } from "../menu-edit-title/menu-edit-title.component";
 
-export const MenuEdit = () => (
-  <Edit>
-    <SimpleForm>
-      <TextInput source="id" disabled />
-      <TextInput source="title" fullWidth />
-      <TextInput source="image" fullWidth />
-      <TextInput source="ingredients" fullWidth />
-      <NumberInput source="price" />
-      <NumberInput source="weight" />
-    </SimpleForm>
-  </Edit>
-);
+export const MenuEdit = () => {
+  return (
+    <Edit title={<MenuEditTitle />}>
+      <SimpleForm>
+        <TextInput source="title" fullWidth label="Назва" />
+        <TextInput source="image" fullWidth label="Фото" />
+        <TextInput source="ingredients" fullWidth label="Інгредієнти" />
+        <NumberInput source="price" label="Ціна в грн." />
+        <NumberInput source="weight" label="Вага в гр." />
+      </SimpleForm>
+    </Edit>
+  );
+};
--- src/modules/menu/components/menu-list/menu-list.component.tsx
+++ src/modules/menu/components/menu-list/menu-list.component.tsx
@@ -1,10 +1,29 @@
-import { Datagrid, List, NumberField, TextField } from "react-admin";
+import {
+  Datagrid,
+  EditButton,
+  FunctionField,
+  List,
+  TextField,
+  TextInput,
+} from "react-admin";
+import { MenuListExpand } from "../menu-list-expand/menu-list-expand.component";
+import { Menu } from "@app/core/types";
+
+const filters = [<TextInput source="title" label="Пошук по назві" />];
 
 export const MenuList = () => (
-  <List>
-    <Datagrid rowClick="edit">
-      <TextField source="title" />
-      <NumberField source="price" />
+  <List filters={filters}>
+    <Datagrid
+      expand={MenuListExpand}
+      rowClick="expand"
+      bulkActionButtons={false}
+    >
+      <TextField source="title" label="Назва" />
+      <FunctionField
+        label="Ціна"
+        render={(record: Menu) => `${record.price} грн.`}
+      />
+      <EditButton />
     </Datagrid>
   </List>
 );
